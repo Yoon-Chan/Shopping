c7d67ab2970c9d0388b1634517eea34cb0e45838
윤찬 <vsvx13@naver.com>
2024-01-11 14:15:01 +0900

    Feat: TopAppBar 구현 및 mainViewModel 추가

--- presentation/src/main/java/com/example/presentation/ui/MainScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/MainScreen.kt
@@ -4,28 +4,35 @@ import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.presentation.ui.theme.ShoppingTheme
+import com.example.presentation.viewmodel.MainViewModel
 
-sealed class MainNavigationItem(val route: String, val name: String) {
-    data object Main : MainNavigationItem("Main", "Main")
-    data object Category : MainNavigationItem("Category", "Category")
-    data object MyPage : MainNavigationItem("MyPage", "MyPage")
+sealed class MainNavigationItem(val route: String, val name: String, val icon: ImageVector) {
+    data object Main : MainNavigationItem("Main", "Main", Icons.Default.Home)
+    data object Category : MainNavigationItem("Category", "Category", Icons.Default.List)
+    data object MyPage : MainNavigationItem("MyPage", "MyPage", Icons.Default.Settings)
 }
 
 
@@ -39,16 +46,39 @@ fun DefaultPreview() {
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen() {
+fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+            Header(viewModel)
+        },
         bottomBar = {
             MainBottomNavigationBar(navController)
         }
     ) {
         MainNavigationScreen(navController)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header(mainViewModel: MainViewModel) {
+    TopAppBar(
+        title = { Text(text = "My App") },
+        actions = {
+            IconButton(onClick = {
+                mainViewModel.openSearchForm()
+            }) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "SearchIcon")
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
 }
 
 @Composable
@@ -77,17 +107,8 @@ fun MainBottomNavigationBar(navController: NavHostController) {
                         restoreState = true
                     }
                 },
-                icon = { 
-                    when(item){
-                        MainNavigationItem.Main -> {
-                            Icon(Icons.Default.Home, "home") }
-                        MainNavigationItem.Category -> {
-                            Icon(Icons.Default.List, "list")
-                        }
-                        MainNavigationItem.MyPage -> {
-                            Icon(Icons.Default.Settings, "settings")
-                        }
-                    }
+                icon = {
+                    Icon(imageVector = item.icon, contentDescription = item.name)
                 },
                 label = { Text(text = item.name) })
         }
@@ -97,7 +118,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
 
 @Composable
 fun MainNavigationScreen(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = MainNavigationItem.Main.route){
+    NavHost(navController = navController, startDestination = MainNavigationItem.Main.route) {
         composable(MainNavigationItem.Main.route) {
             Text(text = "Hello Main")
         }
