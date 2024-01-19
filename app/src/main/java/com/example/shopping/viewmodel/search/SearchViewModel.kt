09b115da1ab9f60b2f0b6752f1ee4dd87f7337d4
윤찬 <vsvx13@naver.com>
2024-01-20 03:00:33 +0900

    Feat: 검색 화면 필터링 기능 추가

--- app/src/main/java/com/example/shopping/viewmodel/search/SearchViewModel.kt
+++ app/src/main/java/com/example/shopping/viewmodel/search/SearchViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.example.domain.model.Product
+import com.example.domain.model.SearchFilter
 import com.example.domain.model.SearchKeyword
 import com.example.domain.usecase.SearchUseCase
 import com.example.shopping.delegate.ProductDelegate
@@ -22,15 +23,30 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val useCase: SearchUseCase
 ) : ViewModel(), ProductDelegate {
+
+    private val searchManager = SearchManager()
     private val _searchResult = MutableStateFlow<List<ProductVM>>(emptyList())
     val searchResult: StateFlow<List<ProductVM>> = _searchResult.asStateFlow()
     val searchKeywords = useCase.getSearchKeywords()
+    val searchFilters = searchManager.filters
 
     fun search(keyword: String) {
         viewModelScope.launch {
-            useCase.search(SearchKeyword(keyword)).collectLatest {
-                _searchResult.emit(it.map(::convertToProductVM))
-            }
+            searchInternal(keyword)
+        }
+    }
+
+    fun updateFilter(filter: SearchFilter) {
+        viewModelScope.launch {
+            searchManager.updateFilter(filter)
+            searchInternal()
+        }
+    }
+
+    private suspend fun searchInternal(newSearchKeyword: String = "") {
+        useCase.search(searchManager.searchKeyword, searchManager.currentFilters()).collectLatest {
+            if (newSearchKeyword.isNotEmpty()) searchManager.initSearchManager(newSearchKeyword, it)
+            _searchResult.emit(it.map(::convertToProductVM))
         }
     }
 
